6c637a126faf61bba60311cd60a9d0d0a0668ed2
Emil Gaarde <gaardee@gmail.com>
2017-03-14 11:11:14 +0100

    init

---
+++ src/dal/Connector.java
@@ -0,0 +1,44 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/** @author Ronnie Dalsgaard */
+public class Connector {
+    private final String HOST     = "185.121.172.103";
+    private final int    PORT     = 3306;
+    private final String DATABASE = "nizzledk_cdio";
+    private final String USERNAME = "nizzledk_cdio"; 
+    private final String PASSWORD = "P9f7gIA5TqZ2";
+    private Connection connection;
+    
+    public Connector() {
+        try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
+			connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
+			System.out.println("Connected to : " + url);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+    }
+    
+    public Connection getConnection(){
+    	return connection;
+    }
+    
+    public ResultSet doQuery(String query) throws SQLException{
+        Statement stmt = connection.createStatement();
+        ResultSet res = stmt.executeQuery(query);
+        return res;
+    }
+    
+    public void doUpdate(String query) throws SQLException{
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate(query);
+    }
+}
---
+++ src/test01917/Main.java
@@ -0,0 +1,54 @@
+package test01917;
+
+import java.sql.Connection;
+/*import daoimpl01917.MySQLOperatoerDAO;
+import daointerfaces01917.DALException;
+import dto01917.OperatoerDTO;
+*/
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import dal.Connector;
+
+public class Main {
+	public static void main(String[] args) throws SQLException {
+		Connector connector = new Connector();
+		Connection connection = connector.getConnection();
+		Statement st = connection.createStatement();
+        String sql = "insert into rolle (`rolle_id`, `rolle_navn`) VALUES ('2', 'Test')";
+
+        int update = st.executeUpdate(sql);
+		
+		/*System.out.println("Operatoer nummer 3:");
+		MySQLOperatoerDAO opr = new MySQLOperatoerDAO();
+		try { System.out.println(opr.getOperatoer(3)); }
+		catch (DALException e) { System.out.println(e.getMessage()); }
+		
+		System.out.println("Indsaettelse af ny operatoer med opr_id =  4");
+		OperatoerDTO oprDTO = new OperatoerDTO(4,"Don Juan","DJ","000000-0000","iloveyou");
+		try { opr.createOperatoer(oprDTO); }
+		catch (DALException e) { System.out.println(e.getMessage()); }	
+		
+		System.out.println("Operatoer nummer 4:");
+		try { System.out.println(opr.getOperatoer(4)); }
+		catch (DALException e) { System.out.println(e.getMessage()); }
+		
+		System.out.println("Opdatering af initialer for operatoer nummer 4");
+		oprDTO.setIni("DoJu");
+		try { opr.updateOperatoer(oprDTO); }
+		catch (DALException e) { System.out.println(e.getMessage()); }
+		
+		System.out.println("Operatoer nummer 4:");
+		try { System.out.println(opr.getOperatoer(4)); }
+		catch (DALException e) { System.out.println(e.getMessage()); }
+		
+		System.out.println("Alle operatoerer:");
+		try { System.out.println(opr.getOperatoerList()); }
+		catch (DALException e) { System.out.println(e.getMessage()); }
+		
+		System.out.println("Operatoer nummer 5:");
+		try { System.out.println(opr.getOperatoer(5)); }
+		catch (DALException e) { System.out.println(e.getMessage()); }		
+		*/
+	}
+}
